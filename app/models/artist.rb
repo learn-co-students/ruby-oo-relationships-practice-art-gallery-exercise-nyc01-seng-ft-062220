27e42ba479e269171c5a77a9766ef89ac505f090
elandau91 <eli.b.landau@gmail.com>
2020-06-29 15:47:48 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,53 @@
+require "pry"
+
 class Artist
 
-  attr_reader :name, :years_experience
+  attr_accessor :years_experience
+  attr_reader :name
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|p| p.artist == self}
+  end
+
+  def galleries
+    self.paintings.map {|p| p.gallery}
+  end
+
+  def cities
+    self.galleries.map {|g| g.city}
+  end
+
+  def self.total_experience
+    total = self.all.map {|a| a.years_experience}
+    total.sum(0.0)
+  end
+
+  def ppy
+    self.years_experience.to_f / self.paintings.count.to_f
+  end
+
+  def self.most_prolific
+    the_ppy = self.all.map {|a| a.ppy}
+     
+    self.all.select {|a| a.ppy == the_ppy.min}
+  end
+
+  def create_painting (title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,21 @@
 require_relative '../config/environment.rb'
+require "pry"
+
+
+van_gogh = Artist.new("Van Gogh", 35)
+monet = Artist.new("Monet", 30)
+pollack = Artist.new("Pollack", 20)
+matisse = Artist.new("Matisse", 50)
+
+met = Gallery.new("The Met", "NYC")
+louvre = Gallery.new("The Louvre", "Paris")
+lacma = Gallery.new("LACMA", "LA")
+
+starry = Painting.new("Starry Night", 100, van_gogh, met)
+sp = Painting.new("Self Portrait", 50, van_gogh, louvre)
+pond = Painting.new("Pond", 75, monet, louvre)
+number_nine = Painting.new("9", 40, pollack, lacma)
+leaves = Painting.new("Leaves", 60, matisse, met)
 
 
 
