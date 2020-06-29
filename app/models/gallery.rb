3ea2d98b7ff2a0d4eb2f4721614709844abbb892
dukhyunko93 <dukhyunko93@gmail.com>
2020-06-29 16:57:46 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,29 @@
 class Gallery
 
   attr_reader :name, :city
-
+  
+  @@all = []
+  
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select{|id| id.gallery == self}
+  end
+  
+  def artists
+    paintings.map {|id| id.artist}
+  end
+
+  def artist_name
+    artists.map{|id| id.name}.uniq
+  end
+
+  def most_expensive_painting
+    paintings.max_by {|id| id.price}
   end
 
 end
