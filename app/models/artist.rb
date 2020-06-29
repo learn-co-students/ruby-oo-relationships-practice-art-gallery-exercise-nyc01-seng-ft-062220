08f8d7a1774aca71b9022cda3437f2e2968d13a4
Jeff-Adler <Jeffm.adler@gmail.com>
2020-06-29 14:23:57 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,37 @@
 class Artist
 
+  @@all = []
   attr_reader :name, :years_experience
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
   end
 
 end
+
+
+# Returns an array of all the artists
+# Artist#paintings
+
+# Returns an array all the paintings by an artist
+# Artist#galleries
+
+# Returns an array of all the galleries that an artist has paintings in
+# Artist#cities
+
+# Return an array of all cities that an artist has paintings in
+# Artist.total_experience
+
+# Returns an integer that is the total years of experience of all artists
+# Artist.most_prolific
+
+# Returns an instance of the artist with the highest amount of paintings per year of experience.
+# Artist#create_painting
+
+# Given the arguments of title, price and gallery, creates a new painting belonging to that artist
